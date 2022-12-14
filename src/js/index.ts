afc5fbf48c010be415a814d0e3874c2db1870479
Gideon Ritter <gideon.ritter@taikomedia.com>
2022-12-14 19:35:25 +0100

    Export SubmitHandler.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,3 @@
-export { default as BaseForm } from "./Components/BaseForm";
+export { default as BaseForm, SubmitHandler } from "./Components/BaseForm";
 export { default as Form } from "./Components/Form";
 export * from "./Hooks/LoadingContext";
