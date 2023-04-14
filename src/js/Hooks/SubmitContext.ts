03325c7105516b2edc9d2cd2964d14fac0700fa5
Gideon Ritter <gideon.ritter@enymo.com>
2023-04-14 12:13:39 +0200

    Minor fix.

--- src/js/Hooks/SubmitContext.ts
+++ src/js/Hooks/SubmitContext.ts
@@ -2,4 +2,4 @@ import React, { createContext, useContext } from "react";
 
 const Context = createContext<(name: string) => (e: React.BaseSyntheticEvent) => Promise<void>>(null);
 export const SubmitProvider = Context.Provider;
-export const useSubmit = () => useContext(Context);
+export const useSubmit = (submitButton: string) => useContext(Context)(submitButton);
